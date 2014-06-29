f3de960b12ff766adc881a0391378d3e19a0e706
Joel Auterson <joel.auterson@googlemail.com>
2014-06-29 19:55:36 +0100

    Wrote some more opcodes, removed t-timer as not really needed.

--- CPU.py
+++ CPU.py
@@ -13,7 +13,7 @@ class CPU:
 
   def __init__ (self):
     #Clock
-    self.ct, self.cm = [0]*2
+    self.ct = 0
 
     #Registers
     self.a = 0 # 8 bit
@@ -24,7 +24,7 @@ class CPU:
     self.h = 0
     self.l = 0
     self.pc, self.sp = [0]*2 # 16 bit
-    self.m, self.t = [0]*2   # Last clock
+    self.m = 0               # Last clock
 
     #Flags
     self.fZero = False
@@ -42,11 +42,65 @@ class CPU:
   #0x00 - NOP
   @opcode
   def nop (self):
+    print "NOP"
     self.m = 1
-    self.t = 4
 
-  #0x01 - LD BC, d16
+  #0x01 - LD BC, d16 TODO
   @opcode
   def ldbc16 (self):
-    #do stuff
-    return
+    print "LD BC, d16"
+    self.m = 3
+
+  #0x02 - LD (BC) A TODO
+  @opcode
+  def ldbca (self):
+    print "LD BC A"
+    self.m = 2
+
+  #0x03 - INC BC TODO
+  @opcode
+  def incbc (self):
+    print "INC BC"
+    self.m = 2
+
+  #0x04 - INC B TODO
+  @opcode
+  def incb (self):
+    print "INC B"
+    self.m = 1
+
+  #0x05 - DEC B TODO
+  @opcode
+  def decb (self):
+    print "DEC B"
+    self.m = 1
+
+  #0x06 - LD B, d8 TODO
+  @opcode
+  def ldb8 (self):
+    print "LD B, d8"
+    self.m = 2
+
+  #0x07 - RLCA TODO
+  @opcode
+  def rlca (self):
+    print "RLCA"
+    self.m = 1
+
+  #0x08 - LD (a16), SP TODO
+  @opcode
+  def lda16sp (self):
+    print "LD (a16), SP"
+    self.m = 5
+
+  #0x09 - ADD HL, BC
+  @opcode
+  def addhlbc (self):
+    print "ADD HL, BC"
+    self.m = 2
+
+  #0x0A - LD A, (BC)
+  @opcode
+  def ldabc (self):
+    print "LD A, (BC)"
+    self.m = 2
--- Emulator.py
+++ Emulator.py
@@ -4,3 +4,11 @@ import sys
 romFile = sys.argv[1]
 cpu = CPU.CPU()
 cpu.mmu.load(romFile)
+while (True):
+  cpu.pc += 1
+  op = cpu.mmu.rb(cpu.pc)
+  print "Op: " + str(op)
+  CPU.opcodes[op](cpu)
+  cpu.pc &= 65535
+  cpu.cm += cpu.m
+  cpu.ct += cpu.t
